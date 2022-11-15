64f82c487ea72e23f1d502164f23a4467cc14998
Peter Broadbent <103116958+PetyrB2@users.noreply.github.com>
2022-11-15 12:01:04 +0000

    Added Bank Demo Class. Created demo accounts and used the deposit method and withdraw method to run the code

--- src/BankAccount/BankAccountDemo.java
+++ src/BankAccount/BankAccountDemo.java
@@ -3,8 +3,42 @@ package BankAccount;
 public class BankAccountDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// Make Accounts
+		BankAccount myAccount  = new BankAccount("Peter B", 150000);
+		BankAccount nicsAccount  = new BankAccount("Nichola Sutton", 250);
+	
+		// Check Balance on Nics Account
+		System.out.println("Account Name: " + nicsAccount.getOwner());
+		System.out.println("Balance: " + nicsAccount.getBalance());
+		System.out.println("");
+		
+		nicsAccount.Deposit(500); // Deposit 
+		System.out.println("Account Name: " + nicsAccount.getOwner());
+		System.out.println("Balance: " + nicsAccount.getBalance());
+		System.out.println("");
+		
+		nicsAccount.Withdraw(1000); // Trigger Warning !!
+		System.out.println("Account Name: " + nicsAccount.getOwner());
+		System.out.println("Balance: " + nicsAccount.getBalance());
+		System.out.println("");
+		
+		// My Account
+		
+		System.out.println("Account Name: " + myAccount.getOwner());
+		System.out.println("Balance: " + myAccount.getBalance());
+		System.out.println("");
+		
+		myAccount.Deposit(750); // Deposit 
+		System.out.println("Account Name: " + myAccount.getOwner());
+		System.out.println("Balance: " + myAccount.getBalance());
+		System.out.println("");
+		
+		myAccount.Withdraw(750); // Withdraw 
+		System.out.println("Account Name: " + myAccount.getOwner());
+		System.out.println("Balance: " + myAccount.getBalance());
+		System.out.println("");
+		
 	}
 
 }
